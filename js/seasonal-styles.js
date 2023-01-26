e2b7a90fb795d8f576c5f59ea90d0c29ac096329
Charlene Tolentino <ctolentino@sea.yearup.org>
2023-01-26 13:43:43 -0800

    FINAL 1.1

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -12,35 +12,35 @@ $("document").ready(function(){
                 $("#logo").attr("src","images/spring.gif");
                 $("#wear").attr("src","images/spring-wear.jpg");
                 $("html").css("background-color","#2B7129");
-                $("header>h3").text("Spring means new things!")
+                $(".heading").text("Spring means new things!")
                 break;
 
             case "summer":
                 $("#logo").attr("src","images/summer.gif");
                 $("#wear").attr("src","images/summer-wear.jpg");
                 $("html").css("background-color","#EBA52B");
-                $("heading").text("Jump into Summer!")
+                $(".heading").text("Jump into Summer!")
                 break;
 
             case "fall":
                 $("#logo").attr("src","images/fall.gif");
                 $("#wear").attr("src","images/fall-wear.jpg");
                 $("html").css("background-color","#A81124");
-                $("heading").text("Fall into Autumn!")
+                $(".heading").text("Fall into Autumn!")
                 break;
 
             case "winter":
                 $("#logo").attr("src","images/winter.gif");
                 $("#wear").attr("src","images/winter-wear.jpg");
                 $("html").css("background-color","#005393");
-                $("heading").text("Bundle up for Winter!")
+                $(".heading").text("Bundle up for Winter!")
                 break;
             
             default:
                 $("#logo").attr("src","images/four-seasons.gif");
                 $("#wear").attr("src","images/300x400.png");
                 $("html").css("background-color","#ccc");
-                $("heading").text("Jump into Spring!")
+                $(".heading").text("Outfitter for All Seasons!")
         }
     });
 
